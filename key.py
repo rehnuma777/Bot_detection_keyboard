5cbdd7512f1b358b27621e7758adafb8e6821ac7
rehnuma777 <43914467+rehnuma777@users.noreply.github.com>
2021-07-15 20:10:05 -0230

    Update key.py

--- key.py
+++ key.py
@@ -4,27 +4,28 @@ import time
 import pandas as pd
 import csv
 
-
+from datetime import datetime
 
 #responsible for handling input streams like listening ot controlling
 counter_time = 0
 
+filename = datetime.now().strftime("%Y-%m-%d-%H-%M.csv")
 
 p = []
 r = []
 count = 0
 keydata = 'a'
+#t = time.time()
 global key
-filename = 'log.csv'
-#def writetofile(key):
+
 
 def write_headers():
     write_row('Key pressed','Key released','Time' )
 
-def write_row(keypressed, keyrelease, time):
+def write_row(keypressed, keyreleased, timestamp):
     with open(filename, 'a') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow([keypressed,keyrelease,time])
+        csv_writer.writerow([keypressed,keyreleased,timestamp])
 
 
 
@@ -32,20 +33,18 @@ def on_press(key):
         #try:
         global t
         keydata = str(key)
-
         keydata = keydata.replace("'","")
         counter_t = round(time.time()-t, 2)
-        #p.append(counter_t)
-        #time = keydata.time
-        write_row(keydata,'', counter_t)
-        
+        write_row(keydata,'None', counter_t)
+
+
 
 def  on_release(key):
     global t
     counter_time = 0
     counter_time = round(time.time()-t, 2)
     #r.append(counter_time)
-    write_row(' ',key, counter_time)
+    write_row('None',key, counter_time)
 
      #print('{0} released'.format(key))
     if key == keyboard.Key.esc:
